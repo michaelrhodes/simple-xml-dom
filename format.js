17b7661e7daad45df36a1f559a7aef15ba522a7f
michaelrhodes <spam@michaelrhod.es>
2015-02-27 19:31:54 +1000

    Make format collect whitespace for each declaration. Rename format variables, for clarity.

--- format.js
+++ format.js
@@ -1,17 +1,19 @@
-var wsdec = /<(!|\?)[^>]+>(\s+)/g
-var dec = /(<(!|\?)[^>]+>)/g
+var declarationTrail = /<(!|\?)[^>]+>(\s+)/g
+var declaration = /(<(!|\?)[^>]+>)/g
 
 module.exports = function (a, b) {
-  var ws = (function () {
+  var spaces = (function () {
     if (typeof b !== 'string') return
-
-    var ws = []
+    var spaces = []
     var match
-
-    while (match = wsdec.exec(b), !!match)
-      if (match[2]) ws.push(match[2])
-    return ws[0]
+    while (match = declarationTrail.exec(b), !!match)
+      if (match[2]) spaces.push(match[2])
+    return spaces
   })() || '\n'
 
-  return a.replace(dec, '$1' + ws)
+  var line  = 0
+  var last = spaces[spaces.length - 1]
+  return a.replace(declaration, function (m, $1) {
+    return $1 + (spaces[line++] || last)
+  })
 }
